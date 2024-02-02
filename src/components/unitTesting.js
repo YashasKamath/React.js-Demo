f32c5ba89084f22b5f04d9e5b07a53fe04536719
Yashas Kamath <yashaskamath81@gmail.com>
2024-02-02 19:40:55 +0530

    deleted unitTesting.js

--- src/components/unitTesting.js
+++
@@ -1,8 +0,0 @@
-function ratingCalculator(farmers){
-    var sum = 0.0, numberOfFarmers = farmers.length
-    farmers.map(farmer => sum += Number(farmer.rating))
-    const avgRating = sum / numberOfFarmers
-    return avgRating
-}
-
-export default ratingCalculator
